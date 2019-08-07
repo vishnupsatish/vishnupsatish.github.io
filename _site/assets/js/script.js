b26535b9c4feb7816274a5631f0dc1ca64e4d018
Vishnu Satish <vishnupavan.satish@gmail.com>
2019-08-07 18:42:47 -0400

    Create projects Page

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -11,7 +11,7 @@
 
 // This function will run a throttled script every 300 ms
 var checkHeader = _.throttle(() => { 
-    console.log('checkHeader');
+    //console.log('checkHeader');
 
     // Detect scroll position
     let scrollPosition = Math.round(window.scrollY);
@@ -221,4 +221,36 @@ function bodyOnload() {
     }
 }
 
+(function() {
 
+    'use strict';
+  
+    // define variables
+    var items = document.querySelectorAll(".timeline li");
+  
+    // check if an element is in viewport
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+  
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+  
+  })();
